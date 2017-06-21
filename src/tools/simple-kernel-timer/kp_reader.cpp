5f41671d063c52bdf5bf534f6a311073ab8c6dcd
Clay Hughes <chughes@sandia.gov>
2017-06-21 09:48:32 -0600

    Added demangle

--- src/tools/simple-kernel-timer/kp_reader.cpp
+++ src/tools/simple-kernel-timer/kp_reader.cpp
@@ -6,6 +6,11 @@
 #include <algorithm>
 #include <map>
 
+#include <iostream>
+#include <string>
+#include <array>
+#include <memory>
+
 #include "kp_kernel_info.h"
 
 bool compareKernelPerformanceInfo(KernelPerformanceInfo* left, KernelPerformanceInfo* right) {
@@ -24,6 +29,21 @@ int find_index(std::vector<KernelPerformanceInfo*>& kernels,
 	}
 	
 	return -1;
+}
+
+std::string runCmd(const char* cmd)
+{
+	std::array<char, 256> buffer;
+	std::string retVal;
+	std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+
+	while(!feof(pipe.get()))
+	{
+		if(fgets(buffer.data(), 256, pipe.get()) != NULL)
+			retVal = retVal + buffer.data();
+	}
+
+	return retVal;
 }
 
 int main(int argc, char* argv[]) {
@@ -92,16 +112,23 @@ int main(int argc, char* argv[]) {
 
 	for(int i = 0; i < kernelInfo.size(); i++) {
 		const double callCountDouble = (double) kernelInfo[i]->getCallCount();
+		
+		std::string mangledName = "_Z" + std::string(kernelInfo[i]->getName());	
+		//std::cout << "Name:  " << mangledName << "\n";
+		std::string mooCows = "c++filt " + mangledName;
+		mangledName = runCmd(mooCows.c_str());
+		//std::cout << "Name-D:  " << mangledName << "\n";
+
 
                 if(fixed_width)
-		printf("%100s%c%15.5f%c%20" PRIu64 "%c%15.5f%c%7.3f%c%7.3f\n", kernelInfo[i]->getName(),
+		printf("%100s%c%15.5f%c%20" PRIu64 "%c%15.5f%c%7.3f%c%7.3f\n", mangledName.c_str(), //kernelInfo[i]->getName(),
 			delimiter,kernelInfo[i]->getTime(),
 			delimiter,kernelInfo[i]->getCallCount(),
 			delimiter,kernelInfo[i]->getTime() / callCountDouble,
 			delimiter,(kernelInfo[i]->getTime() / totalKernelsTime) * 100.0,
 			delimiter,(kernelInfo[i]->getTime() / totalExecuteTime) * 100.0 );
                 else
-                printf("%s%c%f%c%" PRIu64 "%c%f%c%f%c%f\n", kernelInfo[i]->getName(),
+                printf("%s%c%f%c%" PRIu64 "%c%f%c%f%c%f\n", mangledName.c_str(), //kernelInfo[i]->getName(),
                         delimiter,kernelInfo[i]->getTime(),
                         delimiter,kernelInfo[i]->getCallCount(),
                         delimiter,kernelInfo[i]->getTime() / callCountDouble,
